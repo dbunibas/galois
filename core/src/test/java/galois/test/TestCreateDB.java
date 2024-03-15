8041f4e9525f9e2f6f4e51e07c536ff5bc49607a
Dario Satriani <dario.satriani.code@gmail.com>
2024-03-15 17:00:37 +0100

    Added TestCreateDB

---
+++ core/src/test/java/galois/test/TestCreateDB.java
@@ -0,0 +1,51 @@
+package galois.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import speedy.model.algebra.operators.ICreateTable;
+import speedy.model.algebra.operators.sql.SQLCreateTable;
+import speedy.model.database.Attribute;
+import speedy.model.database.dbms.DBMSDB;
+import speedy.persistence.relational.AccessConfiguration;
+
+import java.util.List;
+
+public class TestCreateDB {
+    @Test
+    public void testCreateDummyActorsDB() {
+        String driver = "org.postgresql.Driver";
+        String uri = "jdbc:postgresql:speedy_dummy_actors";
+        String schemaName = "target";
+        String username = "pguser";
+        String password = "pguser";
+
+        AccessConfiguration accessConfiguration = new AccessConfiguration();
+        accessConfiguration.setDriver(driver);
+        accessConfiguration.setUri(uri);
+        accessConfiguration.setSchemaName(schemaName);
+        accessConfiguration.setLogin(username);
+        accessConfiguration.setPassword(password);
+
+        DBMSDB db = new DBMSDB(accessConfiguration);
+        Assertions.assertNotNull(db);
+        db.getInitDBConfiguration().setInitDBScript(createSchema(schemaName));
+        db.initDBMS();
+
+        String tableName = "actor";
+        List<Attribute> attributes = List.of(
+                new Attribute(tableName, "name", "string"),
+                new Attribute(tableName, "sex", "string")
+        );
+
+        ICreateTable tableGenerator = new SQLCreateTable();
+        tableGenerator.createTable(tableName, attributes, db);
+
+        Assertions.assertEquals(db.getTableNames().size(), 1);
+    }
+
+    private String createSchema(String schemaName) {
+        StringBuilder sb = new StringBuilder();
+        if (schemaName == null || schemaName.isEmpty()) return "";
+        return sb.append("create schema ").append(schemaName).append(";").toString();
+    }
+}
