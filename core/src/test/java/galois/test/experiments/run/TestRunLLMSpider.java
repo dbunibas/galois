9c811107c3316e39b6325b438450f155de4b7381
sararst <sararosato.r@gmail.com>
2024-05-22 15:05:57 +0200

    Test run

---
+++ core/src/test/java/galois/test/experiments/run/TestRunLLMSpider.java
@@ -0,0 +1,19 @@
+package galois.test.experiments.run;
+
+import galois.test.experiments.Experiment;
+import galois.test.experiments.ExperimentResults;
+import galois.test.experiments.json.parser.ExperimentParser;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+@Slf4j
+public class TestRunLLMSpider {
+    @Test
+    public void testContinentsLlama3Experiment() throws IOException {
+        Experiment experiment = ExperimentParser.loadAndParseJSON("/continents/continents-llama3-experiment.json");
+        ExperimentResults results = experiment.execute();
+        log.info("{}", results);
+    }
+}
