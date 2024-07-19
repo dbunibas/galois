58cd01c54bc8de1e3ac118724a2b02828c01bc72
Donatello Santoro <donatello.santoro@svelto.tech>
2024-07-19 12:45:30 +0200

    Fixed gradle file

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -39,6 +39,8 @@ dependencies {
 
     implementation("org.apache.commons:commons-csv:1.11.0")
 
+    implementation("org.apache.poi:poi-ooxml:5.2.3")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
