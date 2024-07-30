b3ba90a2e8733f94a5222fa07f05eb96b387f3db
Sara Rosato <sararosato.r@gmail.com>
2024-07-30 18:56:33 +0200

    flight 2 and flight 4

---
+++ core/src/test/java/galois/test/experiments/run/batch/TestRunFlight2Batch.java
@@ -0,0 +1,119 @@
+package galois.test.experiments.run.batch;
+
+import com.galois.sqlparser.SQLQueryParser;
+import galois.optimizer.IOptimizer;
+import galois.optimizer.IndexedConditionPushdownOptimizer;
+import galois.test.experiments.ExperimentResults;
+import galois.test.experiments.json.parser.OptimizersFactory;
+import galois.test.experiments.metrics.IMetric;
+import galois.test.model.ExpVariant;
+import galois.test.utils.ExcelExporter;
+import galois.test.utils.TestRunner;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import speedy.model.algebra.IAlgebraOperator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static speedy.utility.SpeedyUtility.printMap;
+
+@Slf4j
+public class TestRunFlight2Batch {
+
+    private static final String EXP_NAME = "FLIGHT2";
+    private static final String RESULT_FILE_DIR = "src/test/resources/results/";
+    private static final String RESULT_FILE = "flight2-results.txt";
+
+    private static final TestRunner testRunner = new TestRunner();
+    private static final ExcelExporter exportExcel = new ExcelExporter();
+
+    private final List<ExpVariant> variants;
+
+    public TestRunFlight2Batch() {
+        List<String> singleConditionOptimizers = List.of(
+                "AllConditionsPushdownOptimizer",
+                "AllConditionsPushdownOptimizer-WithFilter"
+        );
+        List<String> multipleConditionsOptimizers = List.of(
+                "SingleConditionsOptimizerFactory",
+                "SingleConditionsOptimizerFactory-WithFilter",
+                "AllConditionsPushdownOptimizer",
+                "AllConditionsPushdownOptimizer-WithFilter"
+        );
+
+        ExpVariant q1 = ExpVariant.builder()
+                .queryNum("Q1")
+                .querySql("select a.Abbreviation from target.airlines a where a.Airline='JetBlue Airways'")
+                .prompt("What is the abbreviation of Airline 'JetBlue Airways'?")
+                .optimizers(singleConditionOptimizers)
+                .build();
+
+        ExpVariant q2 = ExpVariant.builder()
+                .queryNum("Q2")
+                .querySql("select count(*) from target.flights f")
+                .prompt("How many flights do we have?")
+                .optimizers(singleConditionOptimizers)
+                .build();
+
+        ExpVariant q3 = ExpVariant.builder()
+                .queryNum("Q3")
+                .querySql("select a.Airline from target.airlines a where a.Abbreviation='UAL'")
+                .prompt("Which airline has abbreviation 'UAL'?")
+                .optimizers(multipleConditionsOptimizers)
+                .build();
+
+        // FIXME: Which Speedy tree can execute this query?
+
+        variants = List.of(q1, q2, q3);
+    }
+
+    @Test
+    public void testCanParseSQLQueries() {
+        SQLQueryParser sqlQueryParser = new SQLQueryParser();
+        for (ExpVariant variant : variants) {
+            log.info("Parsing query {}", variant.getQueryNum());
+            assertDoesNotThrow(() -> {
+                IAlgebraOperator result = sqlQueryParser.parse(variant.getQuerySql());
+                log.info("Parsed result:\n{}", result);
+            });
+        }
+    }
+
+    @Test
+    public void testRunBatch() {
+        List<IMetric> metrics = new ArrayList<>();
+        Map<String, Map<String, ExperimentResults>> results = new HashMap<>();
+        String fileName = exportExcel.getFileName(EXP_NAME);
+        for (ExpVariant variant : variants) {
+            testRunner.execute("/flight_2_data/flight_2-llama3-nl-experiment.json", "NL", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            testRunner.execute("/flight_2_data/flight_2-llama3-sql-experiment.json", "SQL", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            testRunner.execute("/flight_2_data/flight_2-llama3-table-experiment.json", "TABLE", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            testRunner.execute("/flight_2_data/flight_2-llama3-key-experiment.json", "KEY", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            testRunner.execute("/flight_2_data/flight_2-llama3-key-scan-experiment.json", "KEY-SCAN", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            exportExcel.export(fileName, EXP_NAME, metrics, results);
+        }
+        log.info("Results\n{}", printMap(results));
+    }
+
+    @Test
+    public void testSingle() {
+        // TO DEBUG single experiment
+        List<IMetric> metrics = new ArrayList<>();
+        Map<String, Map<String, ExperimentResults>> results = new HashMap<>();
+        ExpVariant variant = variants.get(0);
+        String configPath = "/flight_2_data/flight_2-llama3-table-experiment.json";
+        String type = "TABLE";
+        int indexSingleCondition = 0;
+        IOptimizer allConditionPushdown = OptimizersFactory.getOptimizerByName("AllConditionsPushdownOptimizer");
+        IOptimizer allConditionPushdownWithFilter = OptimizersFactory.getOptimizerByName("AllConditionsPushdownOptimizer-WithFilter");
+        IOptimizer singleConditionPushDownRemoveAlgebraTree = new IndexedConditionPushdownOptimizer(indexSingleCondition, true);
+        IOptimizer singleConditionPushDown = new IndexedConditionPushdownOptimizer(indexSingleCondition, false);
+        IOptimizer nullOptimizer = null; // to execute unomptimize experiments
+        testRunner.executeSingle(configPath, type, variant, metrics, results, singleConditionPushDown);
+    }
+
+}
---
+++ core/src/test/java/galois/test/experiments/run/batch/TestRunFlight4Batch.java
@@ -0,0 +1,119 @@
+package galois.test.experiments.run.batch;
+
+import com.galois.sqlparser.SQLQueryParser;
+import galois.optimizer.IOptimizer;
+import galois.optimizer.IndexedConditionPushdownOptimizer;
+import galois.test.experiments.ExperimentResults;
+import galois.test.experiments.json.parser.OptimizersFactory;
+import galois.test.experiments.metrics.IMetric;
+import galois.test.model.ExpVariant;
+import galois.test.utils.ExcelExporter;
+import galois.test.utils.TestRunner;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import speedy.model.algebra.IAlgebraOperator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static speedy.utility.SpeedyUtility.printMap;
+
+@Slf4j
+public class TestRunFlight4Batch {
+
+    private static final String EXP_NAME = "FLIGHT4";
+    private static final String RESULT_FILE_DIR = "src/test/resources/results/";
+    private static final String RESULT_FILE = "flight4-results.txt";
+
+    private static final TestRunner testRunner = new TestRunner();
+    private static final ExcelExporter exportExcel = new ExcelExporter();
+
+    private final List<ExpVariant> variants;
+
+    public TestRunFlight4Batch() {
+        List<String> singleConditionOptimizers = List.of(
+                "AllConditionsPushdownOptimizer",
+                "AllConditionsPushdownOptimizer-WithFilter"
+        );
+        List<String> multipleConditionsOptimizers = List.of(
+                "SingleConditionsOptimizerFactory",
+                "SingleConditionsOptimizerFactory-WithFilter",
+                "AllConditionsPushdownOptimizer",
+                "AllConditionsPushdownOptimizer-WithFilter"
+        );
+
+        ExpVariant q1 = ExpVariant.builder()
+                .queryNum("Q1")
+                .querySql("select a.name from target.airports a where a.elevation < 50 and a.elevation > -50")
+                .prompt("Find the name of airports whose altitude is between -50 and 50")
+                .optimizers(singleConditionOptimizers)
+                .build();
+
+        ExpVariant q2 = ExpVariant.builder()
+                .queryNum("Q2")
+                .querySql("select max(elevation) from target.airports where country = 'Iceland'")
+                .prompt("What is the maximum elevation of all airports in the country of Iceland?")
+                .optimizers(singleConditionOptimizers)
+                .build();
+
+        ExpVariant q3 = ExpVariant.builder()
+                .queryNum("Q3")
+                .querySql("select name, city, country, elevation from airports where city = 'New York'")
+                .prompt("Find the name, city, country, and altitude (or elevation) of the airports in the city of New York.")
+                .optimizers(multipleConditionsOptimizers)
+                .build();
+
+        // FIXME: Which Speedy tree can execute this query?
+
+        variants = List.of(q1, q2, q3);
+    }
+
+    @Test
+    public void testCanParseSQLQueries() {
+        SQLQueryParser sqlQueryParser = new SQLQueryParser();
+        for (ExpVariant variant : variants) {
+            log.info("Parsing query {}", variant.getQueryNum());
+            assertDoesNotThrow(() -> {
+                IAlgebraOperator result = sqlQueryParser.parse(variant.getQuerySql());
+                log.info("Parsed result:\n{}", result);
+            });
+        }
+    }
+
+    @Test
+    public void testRunBatch() {
+        List<IMetric> metrics = new ArrayList<>();
+        Map<String, Map<String, ExperimentResults>> results = new HashMap<>();
+        String fileName = exportExcel.getFileName(EXP_NAME);
+        for (ExpVariant variant : variants) {
+            testRunner.execute("/flight_2_data/flight_2-llama3-nl-experiment.json", "NL", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            testRunner.execute("/flight_2_data/flight_2-llama3-sql-experiment.json", "SQL", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            testRunner.execute("/flight_2_data/flight_2-llama3-table-experiment.json", "TABLE", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            testRunner.execute("/flight_2_data/flight_2-llama3-key-experiment.json", "KEY", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            testRunner.execute("/flight_2_data/flight_2-llama3-key-scan-experiment.json", "KEY-SCAN", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+            exportExcel.export(fileName, EXP_NAME, metrics, results);
+        }
+        log.info("Results\n{}", printMap(results));
+    }
+
+    @Test
+    public void testSingle() {
+        // TO DEBUG single experiment
+        List<IMetric> metrics = new ArrayList<>();
+        Map<String, Map<String, ExperimentResults>> results = new HashMap<>();
+        ExpVariant variant = variants.get(0);
+        String configPath = "/flight_4_data/flight_4-llama3-table-experiment.json";
+        String type = "TABLE";
+        int indexSingleCondition = 0;
+        IOptimizer allConditionPushdown = OptimizersFactory.getOptimizerByName("AllConditionsPushdownOptimizer");
+        IOptimizer allConditionPushdownWithFilter = OptimizersFactory.getOptimizerByName("AllConditionsPushdownOptimizer-WithFilter");
+        IOptimizer singleConditionPushDownRemoveAlgebraTree = new IndexedConditionPushdownOptimizer(indexSingleCondition, true);
+        IOptimizer singleConditionPushDown = new IndexedConditionPushdownOptimizer(indexSingleCondition, false);
+        IOptimizer nullOptimizer = null; // to execute unomptimize experiments
+        testRunner.executeSingle(configPath, type, variant, metrics, results, singleConditionPushDown);
+    }
+
+}
