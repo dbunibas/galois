69c246bf2b536f9491be97dc506d56f1bc319f80
simonevarriale <varriale.simone.01@gmail.com>
2024-08-02 14:32:53 +0200

    Update on spider geo experiments

--- core/src/test/java/galois/test/experiments/run/batch/TestRunSpiderGeoBatch.java
+++ core/src/test/java/galois/test/experiments/run/batch/TestRunSpiderGeoBatch.java
@@ -47,208 +47,208 @@ public class TestRunSpiderGeoBatch {
 
         ExpVariant q1 = ExpVariant.builder()
                 .queryNum("Q1")
-                .querySql("SELECT area FROM target.state WHERE state_name = \"new mexico\";")
+                .querySql("SELECT area FROM target.usa_state WHERE state_name = 'new mexico';")
                 .prompt("how big is new mexico")
                 .optimizers(singleConditionOptimizers)
                 .build();
 
         ExpVariant q2 = ExpVariant.builder()
                 .queryNum("Q2")
-                .querySql("SELECT city_name FROM city WHERE population > 150000;")
+                .querySql("SELECT city_name FROM usa_city WHERE population > 150000;")
                 .prompt("what are the major cities in the usa")
                 .optimizers(singleConditionOptimizers)
                 .build();
 
         ExpVariant q3 = ExpVariant.builder()
                 .queryNum("Q3")
-                .querySql("SELECT COUNT ( capital ) FROM state WHERE state_name = \"rhode island\";")
+                .querySql("SELECT COUNT ( capital ) FROM usa_state WHERE state_name = 'rhode island';")
                 .prompt("how many capitals does rhode island have")
                 .optimizers(singleConditionOptimizers)
                 .build();
 
         ExpVariant q4 = ExpVariant.builder()
                 .queryNum("Q4")
-                .querySql("SELECT population FROM city WHERE city_name = \"boulder\";")
+                .querySql("SELECT population FROM usa_city WHERE city_name = 'boulder';")
                 .prompt("how many people live in boulder")
                 .optimizers(singleConditionOptimizers)
                 .build();
 
         ExpVariant q5 = ExpVariant.builder()
                 .queryNum("Q5")
-                .querySql("SELECT COUNT ( state_name ) FROM state; ")
+                .querySql("SELECT COUNT ( state_name ) FROM usa_state; ")
                 .prompt("how many states are in the usa")
                 .optimizers(List.of())
                 .build();
 
         ExpVariant q6 = ExpVariant.builder()
                 .queryNum("Q6")
-                .querySql("SELECT lake_name FROM lake WHERE state_name = \"california\";")
+                .querySql("SELECT lake_name FROM usa_lake WHERE state_name = 'california';")
                 .prompt("give me the lakes in california")
                 .optimizers(singleConditionOptimizers)
                 .build();
 
         ExpVariant q7 = ExpVariant.builder()
                 .queryNum("Q7")
-                .querySql("SELECT AVG ( population ) FROM state;")
+                .querySql("SELECT AVG ( population ) FROM usa_state;")
                 .prompt("what is the average population of the us by state")
                 .optimizers(List.of())
                 .build();
 
         ExpVariant q8 = ExpVariant.builder()
                 .queryNum("Q8")
-                .querySql("SELECT COUNT ( river_name ) FROM river;")
+                .querySql("SELECT COUNT ( river_name ) FROM usa_river;")
                 .prompt("how many rivers are there in us")
                 .optimizers(List.of())
                 .build();
 
         ExpVariant q9 = ExpVariant.builder()
                 .queryNum("Q9")
-                .querySql("SELECT LENGTH FROM river WHERE river_name = \"rio grande\";")
+                .querySql("SELECT LENGTH FROM usa_river WHERE river_name = 'rio grande';")
                 .prompt("how long is the rio grande river")
                 .optimizers(singleConditionOptimizers)
                 .build();
 
         ExpVariant q10 = ExpVariant.builder()
                 .queryNum("Q10")
-                .querySql("SELECT traverse FROM river;")
+                .querySql("SELECT traverse FROM usa_river;")
                 .prompt("which states have a river")
                 .optimizers(List.of())
                 .build();
 
-        ExpVariant q11 = ExpVariant.builder()
-                .queryNum("Q11")
-                .querySql("SELECT lake_name FROM lake;")
-                .prompt("name all the lakes of us")
-                .optimizers(List.of())
-                .build();
+//        ExpVariant q11 = ExpVariant.builder()
+//                .queryNum("Q11")
+//                .querySql("SELECT lake_name FROM lake;")
+//                .prompt("name all the lakes of us")
+//                .optimizers(List.of())
+//                .build();
+//
+//        ExpVariant q12 = ExpVariant.builder()
+//                .queryNum("Q12")
+//                .querySql("SELECT traverse FROM river WHERE LENGTH > 750;")
+//                .prompt("what states contain at least one major rivers")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q13 = ExpVariant.builder()
+//                .queryNum("Q13")
+//                .querySql("SELECT state_name FROM city WHERE city_name = 'springfield';")
+//                .prompt("what state is springfield in")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q14 = ExpVariant.builder()
+//                .queryNum("Q14")
+//                .querySql("SELECT lake_name FROM lake WHERE area > 750 AND state_name = 'michigan';")
+//                .prompt("name the major lakes in michigan")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q15 = ExpVariant.builder()
+//                .queryNum("Q15")
+//                .querySql("SELECT lake_name FROM lake WHERE area > 750;")
+//                .prompt("what are the major lakes in united states")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q16 = ExpVariant.builder()
+//                .queryNum("Q16")
+//                .querySql("SELECT state_name FROM city WHERE city_name = 'austin' AND population > 150000;")
+//                .prompt("which states have a major city named austin")
+//                .optimizers(multipleConditionsOptimizers)
+//                .build();
+//
+//        ExpVariant q17 = ExpVariant.builder()
+//                .queryNum("Q17")
+//                .querySql("SELECT COUNT ( state_name ) FROM city WHERE city_name = 'springfield';")
+//                .prompt("how many states have a city named springfield")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q18 = ExpVariant.builder()
+//                .queryNum("Q18")
+//                .querySql("SELECT COUNT ( traverse ) FROM river WHERE LENGTH > 750;")
+//                .prompt("how many states have major rivers")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
 
-        ExpVariant q12 = ExpVariant.builder()
-                .queryNum("Q12")
-                .querySql("SELECT traverse FROM river WHERE LENGTH > 750;")
-                .prompt("what states contain at least one major rivers")
-                .optimizers(singleConditionOptimizers)
-                .build();
+//        ExpVariant q19 = ExpVariant.builder()
+//                .queryNum("Q19")
+//                .querySql("SELECT traverse FROM river WHERE LENGTH = ( SELECT MIN ( LENGTH ) FROM river );")
+//                .prompt("what states does the shortest river run through")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
 
-        ExpVariant q13 = ExpVariant.builder()
-                .queryNum("Q13")
-                .querySql("SELECT state_name FROM city WHERE city_name = \"springfield\";")
-                .prompt("what state is springfield in")
-                .optimizers(singleConditionOptimizers)
-                .build();
+//        ExpVariant q20 = ExpVariant.builder()
+//                .queryNum("Q20")
+//                .querySql("SELECT population FROM city WHERE city_name = 'seattle' AND state_name = 'washington';")
+//                .prompt("what is the population of seattle washington")
+//                .optimizers(multipleConditionsOptimizers)
+//                .build();
+//
+//        ExpVariant q21 = ExpVariant.builder()
+//                .queryNum("Q21")
+//                .querySql("SELECT COUNT ( DISTINCT traverse ) FROM river WHERE LENGTH > 750;")
+//                .prompt("how many states are next to major rivers")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q22 = ExpVariant.builder()
+//                .queryNum("Q22")
+//                .querySql("SELECT DISTINCT capital FROM state;")
+//                .prompt("name the 50 capitals in the usa")
+//                .optimizers(List.of())
+//                .build();
+//
+//        ExpVariant q23 = ExpVariant.builder()
+//                .queryNum("Q23")
+//                .querySql("SELECT COUNT ( river_name ) FROM river WHERE traverse = 'idaho';")
+//                .prompt("how many rivers are in idaho")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q24 = ExpVariant.builder()
+//                .queryNum("Q24")
+//                .querySql("SELECT country_name FROM state WHERE state_name = 'massachusetts';")
+//                .prompt("where is massachusetts")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q25 = ExpVariant.builder()
+//                .queryNum("Q25")
+//                .querySql("SELECT river_name FROM river WHERE LENGTH > 750 AND traverse = 'illinois';")
+//                .prompt("name the major rivers in illinois")
+//                .optimizers(multipleConditionsOptimizers)
+//                .build();
 
-        ExpVariant q14 = ExpVariant.builder()
-                .queryNum("Q14")
-                .querySql("SELECT lake_name FROM lake WHERE area > 750 AND state_name = \"michigan\";")
-                .prompt("name the major lakes in michigan")
-                .optimizers(singleConditionOptimizers)
-                .build();
+//        ExpVariant q26 = ExpVariant.builder()
+//                .queryNum("Q26")
+//                .querySql("SELECT t2.capital FROM state AS t2 JOIN city AS t1 ON t2.state_name = t1.state_name WHERE t1.city_name = 'durham';")
+//                .prompt("name the major rivers in illinois")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
 
-        ExpVariant q15 = ExpVariant.builder()
-                .queryNum("Q15")
-                .querySql("SELECT lake_name FROM lake WHERE area > 750;")
-                .prompt("what are the major lakes in united states")
-                .optimizers(singleConditionOptimizers)
-                .build();
+//        ExpVariant q27 = ExpVariant.builder()
+//                .queryNum("Q27")
+//                .querySql("SELECT state_name FROM state WHERE state_name NOT IN ( SELECT traverse FROM river );")
+//                .prompt("what state has no rivers")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
 
-        ExpVariant q16 = ExpVariant.builder()
-                .queryNum("Q16")
-                .querySql("SELECT state_name FROM city WHERE city_name = \"austin\" AND population > 150000;")
-                .prompt("which states have a major city named austin")
-                .optimizers(multipleConditionsOptimizers)
-                .build();
+//        ExpVariant q28 = ExpVariant.builder()
+//                .queryNum("Q28")
+//                .querySql("SELECT COUNT ( river_name ) FROM river WHERE traverse NOT IN ( SELECT state_name FROM state WHERE capital = 'albany' );")
+//                .prompt("how many rivers do not traverse the state with the capital albany")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
 
-        ExpVariant q17 = ExpVariant.builder()
-                .queryNum("Q17")
-                .querySql("SELECT COUNT ( state_name ) FROM city WHERE city_name = \"springfield\";")
-                .prompt("how many states have a city named springfield")
-                .optimizers(singleConditionOptimizers)
-                .build();
+//        ExpVariant q29 = ExpVariant.builder()
+//                .queryNum("Q29")
+//                .querySql("SELECT t2.capital FROM state AS t2 JOIN city AS t1 ON t2.capital = t1.city_name WHERE t1.population <= 150000;")
+//                .prompt("which capitals are not major cities")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
 
-        ExpVariant q18 = ExpVariant.builder()
-                .queryNum("Q18")
-                .querySql("SELECT COUNT ( traverse ) FROM river WHERE LENGTH > 750;")
-                .prompt("how many states have major rivers")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        ExpVariant q19 = ExpVariant.builder()
-                .queryNum("Q19")
-                .querySql("SELECT traverse FROM river WHERE LENGTH = ( SELECT MIN ( LENGTH ) FROM river );")
-                .prompt("what states does the shortest river run through")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        ExpVariant q20 = ExpVariant.builder()
-                .queryNum("Q20")
-                .querySql("SELECT population FROM city WHERE city_name = \"seattle\" AND state_name = \"washington\";")
-                .prompt("what is the population of seattle washington")
-                .optimizers(multipleConditionsOptimizers)
-                .build();
-
-        ExpVariant q21 = ExpVariant.builder()
-                .queryNum("Q21")
-                .querySql("SELECT COUNT ( DISTINCT traverse ) FROM river WHERE LENGTH > 750;")
-                .prompt("how many states are next to major rivers")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        ExpVariant q22 = ExpVariant.builder()
-                .queryNum("Q22")
-                .querySql("SELECT DISTINCT capital FROM state;")
-                .prompt("name the 50 capitals in the usa")
-                .optimizers(List.of())
-                .build();
-
-        ExpVariant q23 = ExpVariant.builder()
-                .queryNum("Q23")
-                .querySql("SELECT COUNT ( river_name ) FROM river WHERE traverse = \"idaho\";")
-                .prompt("how many rivers are in idaho")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        ExpVariant q24 = ExpVariant.builder()
-                .queryNum("Q24")
-                .querySql("SELECT country_name FROM state WHERE state_name = \"massachusetts\";")
-                .prompt("where is massachusetts")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        ExpVariant q25 = ExpVariant.builder()
-                .queryNum("Q25")
-                .querySql("SELECT river_name FROM river WHERE LENGTH > 750 AND traverse = \"illinois\";")
-                .prompt("name the major rivers in illinois")
-                .optimizers(multipleConditionsOptimizers)
-                .build();
-
-        ExpVariant q26 = ExpVariant.builder()
-                .queryNum("Q26")
-                .querySql("SELECT t2.capital FROM state AS t2 JOIN city AS t1 ON t2.state_name = t1.state_name WHERE t1.city_name = \"durham\";")
-                .prompt("name the major rivers in illinois")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        ExpVariant q27 = ExpVariant.builder()
-                .queryNum("Q27")
-                .querySql("SELECT state_name FROM state WHERE state_name NOT IN ( SELECT traverse FROM river );")
-                .prompt("what state has no rivers")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        ExpVariant q28 = ExpVariant.builder()
-                .queryNum("Q28")
-                .querySql("SELECT COUNT ( river_name ) FROM river WHERE traverse NOT IN ( SELECT state_name FROM state WHERE capital = \"albany\" );")
-                .prompt("how many rivers do not traverse the state with the capital albany")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        ExpVariant q29 = ExpVariant.builder()
-                .queryNum("Q29")
-                .querySql("SELECT t2.capital FROM state AS t2 JOIN city AS t1 ON t2.capital = t1.city_name WHERE t1.population <= 150000;")
-                .prompt("which capitals are not major cities")
-                .optimizers(singleConditionOptimizers)
-                .build();
-
-        variants = List.of(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24, q25, q26, q27, q28, q29);
+        variants = List.of(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10 /*,q11, q12, q13, q14, q15, q16, q17, q18, q20, q21, q22, q23, q24, q25*/);
     }
 
     @Test
