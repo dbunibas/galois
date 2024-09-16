f03bd906b4a9ba31b3ff37ae61bd5f637ca46860
simonevarriale <varriale.simone.01@gmail.com>
2024-09-16 20:14:08 +0200

    Set up of world_1 experiments

--- core/src/test/java/galois/test/experiments/run/batch/TestRunWorld1Batch.java
+++ core/src/test/java/galois/test/experiments/run/batch/TestRunWorld1Batch.java
@@ -45,112 +45,112 @@ public class TestRunWorld1Batch {
                 "AllConditionsPushdownOptimizer-WithFilter"
         );
 
-        ExpVariant q1 = ExpVariant.builder()
-                .queryNum("Q1")
-                .querySql("select c.name from target.city c where c.population > 160000 and c.population < 900000")
-                .prompt("What are the cities whose population is between 160000 and 900000?")
-                .optimizers(singleConditionOptimizers)
-                .build();
+//        ExpVariant q1 = ExpVariant.builder()
+//                .queryNum("Q1")
+//                .querySql("SELECT c.name FROM target.city c WHERE c.population > 160000 AND c.population < 900000;")
+//                .prompt("What are the cities whose population is between 160000 and 900000?")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
 
-        ExpVariant q2 = ExpVariant.builder()
-                .queryNum("Q2")
-                .querySql("select sum(SurfaceArea) from target.country where region='Caribbean'")
-                .prompt("What is the total surface area of the countries in the Caribbean region?")
-                .optimizers(singleConditionOptimizers)
-                .build();
+//        ExpVariant q2 = ExpVariant.builder()
+//                .queryNum("Q2")
+//                .querySql("select sum(surface_area_in_km2) from target.country where region='Caribbean';")
+//                .prompt("What is the total surface area of the countries in the Caribbean region?")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q3 = ExpVariant.builder()
+//                .queryNum("Q3")
+//                .querySql("select distinct country_code_3_letters from target.country_language where language != 'English';")
+//                .prompt("What are the country codes of countries where people use languages other than English?")
+//                .optimizers(multipleConditionsOptimizers)
+//                .build();
+//
+//        ExpVariant q4 = ExpVariant.builder()
+//                .queryNum("Q4")
+//                .querySql("select sum(population), max(gnp) from target.country where continent='Asia';")
+//                .prompt("What is the total population and maximum GNP in Asia?")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
+//
+//        ExpVariant q5 = ExpVariant.builder()
+//                .queryNum("Q5")
+//                .querySql("select sum(population) from target.city where district = 'Gerderland';")
+//                .prompt("How many people live in Gelderland district?")
+//                .optimizers(singleConditionOptimizers)
+//                .build();
 
-        ExpVariant q3 = ExpVariant.builder()
-                .queryNum("Q3")
-                .querySql("select distinct countrycode from target.countrylanguage where language != 'English'")
-                .prompt("What are the country codes of countries where people use languages other than English?")
-                .optimizers(multipleConditionsOptimizers)
-                .build();
-
-        ExpVariant q4 = ExpVariant.builder()
-                .queryNum("Q4")
-                .querySql("select sum(population), max(gnp) from target.country where continent='Asia'")
-                .prompt("What is the total population and maximum GNP in Asia?")
-                .optimizers(multipleConditionsOptimizers)
-                .build();
-
-        ExpVariant q5 = ExpVariant.builder()
-                .queryNum("Q5")
-                .querySql("select sum(population) from target.city where district = 'Gerderland'")
-                .prompt("How many people live in Gelderland district?")
-                .optimizers(multipleConditionsOptimizers)
-                .build();
-
-        ExpVariant q6 = ExpVariant.builder()
-                .queryNum("Q6")
-                .querySql("select avg(LifeExpectancy) from target.country where continent = 'Africa' and GovernmentForm = 'Republic'")
-                .prompt("What is the average life expectancy in African countries that are republics?")
-                .optimizers(multipleConditionsOptimizers)
-                .build();
+//        ExpVariant q6 = ExpVariant.builder()
+//                .queryNum("Q6")
+//                .querySql("select avg(life_expectancy) from target.country where continent = 'Africa' and government_form = 'Republic';")
+//                .prompt("What is the average life expectancy in African countries that are republics?")
+//                .optimizers(multipleConditionsOptimizers)
+//                .build();
 
         ExpVariant q7 = ExpVariant.builder()
                 .queryNum("Q7")
-                .querySql("select count(distinct GovernmentForm) from target.country where continent = 'Africa'")
+                .querySql("select count(distinct government_form) from target.country where continent = 'Africa';")
                 .prompt("How many type of governments are in Africa?")
-                .optimizers(multipleConditionsOptimizers)
+                .optimizers(singleConditionOptimizers)
                 .build();
 
         ExpVariant q8 = ExpVariant.builder()
                 .queryNum("Q8")
-                .querySql("select name from target.country where IndepYear > 1950")
+                .querySql("select name from target.country where independence_year > 1950;")
                 .prompt("What are the names of countries that became independent after 1950?")
-                .optimizers(multipleConditionsOptimizers)
+                .optimizers(singleConditionOptimizers)
                 .build();
 
         ExpVariant q9 = ExpVariant.builder()
                 .queryNum("Q9")
-                .querySql("select avg(gnp), sum(population) from target.country where governmentf orm = 'US Territory'")
+                .querySql("select avg(gnp), sum(population) from target.country where government_form = 'US Territory';")
                 .prompt("What is the average GNP and total population in all nations whose government is US territory?")
+                .optimizers(singleConditionOptimizers)
+                .build();
+
+        ExpVariant q10 = ExpVariant.builder()
+                .queryNum("Q10")
+                .querySql("SELECT T2.Language FROM country AS T1 JOIN country_language AS T2 ON T1.code_3_letters = T2.country_code_3_letters WHERE T1.Name = 'Aruba' ORDER BY Percentage DESC LIMIT 1;")
+                .prompt("Which language is the most popular in Aruba?")
                 .optimizers(multipleConditionsOptimizers)
                 .build();
 
-//        ExpVariant q10 = ExpVariant.builder()
-//                .queryNum("Q10")
-//                .querySql("select t2.language from country as t1, countrylanguage as t2 where t1.code = t2.countrycode on t1.code = t2.countrycode where t1.name = 'Aruba' order by percentage desc limit 1")
-//                .prompt("Which language is the most popular in Aruba?")
-//                .optimizers(multipleConditionsOptimizers)
-//                .build();
+        ExpVariant q11 = ExpVariant.builder()
+                .queryNum("Q11")
+                .querySql("select distinct t1.region from country as t1 join country_language as t2 on t1.code_3_letters = t2.country_code_3_letters where t2.language = 'English' or t2.language = 'Dutch';")
+                .prompt("What are the regions that use English or Dutch?")
+                .optimizers(multipleConditionsOptimizers)
+                .build();
 
-//        ExpVariant q11 = ExpVariant.builder()
-//                .queryNum("Q11")
-//                .querySql("select distinct t1.region from country as t1 join countrylanguage as t2 on t1.code = t2.countrycode where t2.language = 'English' or t2.language = 'Dutch'")
-//                .prompt("What are the regions that use English or Dutch?")
-//                .optimizers(multipleConditionsOptimizers)
-//                .build();
+        ExpVariant q12 = ExpVariant.builder()
+                .queryNum("Q12")
+                .querySql("select t2.language from country as t1 join country_language as t2 on t1.code_3_letters = t2.country_code_3_letters where t1.head_of_state = 'Beatrix' and t2.is_official = 'T';")
+                .prompt("What is the official language spoken in the country whose head of state is Beatrix?")
+                .optimizers(multipleConditionsOptimizers)
+                .build();
 
-//        ExpVariant q12 = ExpVariant.builder()
-//                .queryNum("Q12")
-//                .querySql("select t2.language from country as t1 join countrylanguage as t2 on t1.code = t2.countrycode where t1.headofstate = 'Beatrix' and t2.isofficial = 'T'")
-//                .prompt("What is the official language spoken in the country whose head of state is Beatrix?")
-//                .optimizers(multipleConditionsOptimizers)
-//                .build();
+        ExpVariant q13 = ExpVariant.builder()
+                .queryNum("Q13")
+                .querySql("select count(distinct t1.continent) from country as t1 join country_language as t2 on t1.code_3_letters = t2.country_code_3_letters where t2.language = 'Chinese';")
+                .prompt("What is the number of distinct continents where Chinese is spoken?")
+                .optimizers(multipleConditionsOptimizers)
+                .build();
 
-//        ExpVariant q13 = ExpVariant.builder()
-//                .queryNum("Q13")
-//                .querySql("select count(distinct t1.continent) from country as t1 join countrylanguage as t2 on t1.code = t2.countrycode where t2.language = 'Chinese'")
-//                .prompt("What is the number of distinct continents where Chinese is spoken?")
-//                .optimizers(multipleConditionsOptimizers)
-//                .build();
+        ExpVariant q14 = ExpVariant.builder()
+                .queryNum("Q14")
+                .querySql("select region from country as t1 join city as t2 on t1.code_3_letters = t2.country_code_3_letters where t2.name = 'Kabul';")
+                .prompt("Which region is the city Kabul located in?")
+                .optimizers(multipleConditionsOptimizers)
+                .build();
 
-//        ExpVariant q14 = ExpVariant.builder()
-//                .queryNum("Q14")
-//                .querySql("select region from country as t1 join city as t2 on t1.code = t2.countrycode where t2.name = 'Kabul'")
-//                .prompt("Which region is the city Kabul located in?")
-//                .optimizers(multipleConditionsOptimizers)
-//                .build();
+        ExpVariant q15 = ExpVariant.builder()
+                .queryNum("Q15")
+                .querySql("select count(*) from country as t1 join country_language as t2 on t1.code_3_letters = t2.country_code_3_letters where t1.name = 'Afghanistan' and is_official = 'T';")
+                .prompt("How many official languages does Afghanistan have?")
+                .optimizers(multipleConditionsOptimizers)
+                .build();
 
-//        ExpVariant q15 = ExpVariant.builder()
-//                .queryNum("Q15")
-//                .querySql("select count(*) from country as t1 join countrylanguage as t2 on t1.code = t2.countrycode where t1.name = 'Afghanistan' and isofficial = 'T'")
-//                .prompt("How many official languages does Afghanistan have?")
-//                .optimizers(multipleConditionsOptimizers)
-//                .build();
-
-        variants = List.of(q1, q2, q3, q4, q5, q6, q7, q8, q9);
+        variants = List.of(  q7, q8, q9, q10, q11, q12, q13, q14, q15);
     }
 
     @Test
@@ -174,7 +174,7 @@ public class TestRunWorld1Batch {
             testRunner.execute("/world_1_data/world1-llama3-nl-experiment.json", "NL", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
             testRunner.execute("/world_1_data/world1-llama3-sql-experiment.json", "SQL", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
             testRunner.execute("/world_1_data/world1-llama3-table-experiment.json", "TABLE", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
-            testRunner.execute("/world_1_data/world1-llama3-key-experiment.json", "KEY", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
+//            testRunner.execute("/world_1_data/world1-llama3-key-experiment.json", "KEY", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
             testRunner.execute("/world_1_data/world1-llama3-key-scan-experiment.json", "KEY-SCAN", variant, metrics, results, RESULT_FILE_DIR, RESULT_FILE);
             exportExcel.export(fileName, EXP_NAME, metrics, results);
         }
