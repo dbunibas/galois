72f0bd39a4146ac65aa8aef1e44e054024081a49
Dario Satriani <dario.satriani.code@gmail.com>
2025-08-22 16:46:06 +0200

    Added Speedy as a submodule

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 rootProject.name = "galois"
 
-val speedyRoot = settings.extra.properties["speedyRoot"]
+//val speedyRoot = settings.extra.properties["speedyRoot"]
+val speedyRoot = "./Speedy"
 
 include(":speedy-core", ":jep")
 project(":speedy-core").projectDir = File("${speedyRoot}/core")
