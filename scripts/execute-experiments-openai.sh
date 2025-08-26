39a59342edf600b09126dd068bd647255a33c7ac
Dario Satriani <dario.satriani.code@gmail.com>
2025-08-26 14:48:36 +0200

    Added warning messages to scripts

--- scripts/execute-experiments-openai.sh
+++ scripts/execute-experiments-openai.sh
@@ -1,5 +1,10 @@
-echo "TODO: warning..."
-read -p "Continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo "Before continuing!"
+echo
+echo "This script will execute *all* the experiments of the “Logical and Physical Optimizations for SQL Query Execution over Large Language Models” using OpenAI as the LLM provider."
+echo
+echo "The model to use should have been configured in the <configuration.properties> file using the <openai.model-name> key. Also, the OpenAI api key should have been properly added to the <configuration.properties> file using the <openai.api-key> key."
+echo
+read -p "Start the execution of *all* the experiments? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 cd ../executors
 names=`ls ./*-openai.sh`
--- scripts/execute-experiments-togetherai.sh
+++ scripts/execute-experiments-togetherai.sh
@@ -1,5 +1,10 @@
-echo "TODO: warning..."
-read -p "Continue? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+echo "Before continuing!"
+echo
+echo "This script will execute *all* the experiments of the “Logical and Physical Optimizations for SQL Query Execution over Large Language Models” using TogetherAI as the LLM provider."
+echo
+echo "The model to use should have been configured in the <configuration.properties> file using the <togetherai.model> key. Also, the TogetherAI api key should have been properly added to the <configuration.properties> file using the <togetherai.api-key> key."
+echo
+read -p "Start the execution of *all* the experiments? (y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 cd ../executors
 names=`ls ./*-togetherai.sh`
