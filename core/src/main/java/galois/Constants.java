84811128411cf0e37d167863043937c38cf74e6b
Dario Satriani <dario.satriani.code@gmail.com>
2025-10-14 11:34:32 +0200

    fix: Constants.java

--- core/src/main/java/galois/Constants.java
+++ core/src/main/java/galois/Constants.java
@@ -1,35 +1,9 @@
 package galois;
 
-import dev.langchain4j.model.openai.OpenAiChatModelName;
-import dev.langchain4j.store.embedding.filter.logical.Or;
 import galois.llm.models.togetherai.TogetherAIConstants;
 
 public class Constants {
-
-    public static final String LLM_MODEL = galois.Constants.MODEL_QWEN;
-    public static final String EXPORT_EXCEL_PATH = "~/test-results/";
-
-    public static final String SELECTED_MODEL = Constants.TOGETHERAI_MODEL; //Or Constants.OPEN_AI_CHAT_MODEL_NAME;
-
-    public static final String OPEN_AI_API_KEY = "YOUR_API_KEY";
-    public static final OpenAiChatModelName OPEN_AI_CHAT_MODEL_NAME = OpenAiChatModelName.GPT_4_O_MINI;
-//    public static final String OPEN_AI_CHAT_MODEL_NAME = OpenAiChatModelName.GPT_4_O_MINI.toString();
-//    public static final String OPEN_AI_CHAT_MODEL_NAME = "gpt-4.1-mini";
-    public static final String OPEN_AI_CHAT_MODEL_NAME = "gpt-4.1";
-    public static final String GEMINI_CHAT_MODEL_NAME = "gemini-2.5-flash-lite";
-//    public static final String GEMINI_CHAT_MODEL_NAME = "gemini-2.5-flash";
-    
-    public static final int WAIT_TIME_MS_TOGETHERAI = 40; // for tier-2 40 for tier-1 110 otherwise 1000 for unpaid tier
-//    public static final int WAIT_TIME_MS_TOGETHERAI = 1000; // for tier-2 40 for tier-1 110 otherwise 1000 for unpaid tier
-//    public static final int WAIT_TIME_MS_TOGETHERAI = 6000; // for tier-2 40 for tier-1 110 otherwise 1000 for unpaid tier
-//    public static final String TOGETHERAI_MODEL = TogetherAIConstants.MODEL_GEMMA_2_9B;
-//    public static final String TOGETHERAI_MODEL = TogetherAIConstants.MODEL_DEEPSEEK_R1_DISTIL_LLAMA_70B;
-//    public static final String TOGETHERAI_MODEL = TogetherAIConstants.MODEL_QWEN3_235B;
-//    public static final String TOGETHERAI_MODEL = TogetherAIConstants.MODEL_LLAMA3_3_70B;
-    public static final String TOGETHERAI_MODEL = TogetherAIConstants.MODEL_LLAMA3_1_8B;
-//    public static final String TOGETHERAI_MODEL = TogetherAIConstants.MODEL_QWEN_2_5_7B;
-//    public static final String TOGETHERAI_MODEL = TogetherAIConstants.MODEL_MISTRAL_0_3_7B;
-
+    public static final String LLM_MODEL = Constants.MODEL_LLAMA3;
 
     // ----- TOGETHER AI CONFIG --------
     public static final String TOGETHERAI_API = "<YOUR_API_KEY>";
@@ -40,6 +14,8 @@ public class Constants {
     public static final String OPEN_AI_API_KEY = "<YOUR_API_KEY>";
     public static final String OPEN_AI_CHAT_MODEL_NAME = "gpt-4.1";
 
+    // ----- GEMINI CONFIG --------
+    public static final String GEMINI_CHAT_MODEL_NAME = "gemini-2.5-flash-lite";
 
     // ----- OTHER CONFIG --------
     public static final String OLLAMA_MODEL = "llama3.1:70b";
@@ -54,9 +30,8 @@ public class Constants {
 
     public static final String MODEL_KIMI = "kimi";
 
-    public static final String LLM_MODEL = Constants.MODEL_LLAMA3;
-
-
     public static final boolean CACHE_ENABLED = false;
     public static final String CACHE_DIR = "/Temp/cache";
+
+    public static final String EXPORT_EXCEL_PATH = "~/test-results/";
 }
