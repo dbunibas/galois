d738b508e1d4f94b888f18ac0f2b548bfd3a3bc9
MattiaSernia <mattiasernia.ms@gmail.com>
2025-12-08 14:45:13 +0100

    Addition of the Dog Dataset

--- core/src/test/java/galois/test/evaluation/TestEvaluationDog.java
+++ core/src/test/java/galois/test/evaluation/TestEvaluationDog.java
@@ -24,13 +24,13 @@ import static galois.test.evaluation.SchemaLoader.loadSchemaInExperimentFolder;
 import static galois.test.utils.TestUtils.toTupleList;
 
 @Slf4j
-public class TestEvaluationArtists {
+public class TestEvaluationDog {
     private static final IUserDefinedFunctionFactory GALOIS_UDF_FACTORY = new GaloisUDFFactory();
 
     // Experiment name
     private static final String EXPERIMENT_NAME = "SemBenchMovies";
     // Experiment folder path starting from resources
-    private static final String EXPERIMENT_FOLDER_PATH = "/evaluation/kaggle-artist";
+    private static final String EXPERIMENT_FOLDER_PATH = "/evaluation/kaggle-dog";
 
     private static final String RESULT_FILE_DIR = "src/test/evaluation/results/";
     private static final String RESULT_FILE = "movie-reviews-results.txt";
@@ -55,21 +55,21 @@ public class TestEvaluationArtists {
         SchemaDatabase schema = loadSchemaInExperimentFolder(EXPERIMENT_FOLDER_PATH);
         database = connectToPostgres(schema.getDbName(), "public", "pguser", "pguser");
     //    database = connectToMainMemoryCSV(TestEvaluation.class.getResource(EXPERIMENT_FOLDER_PATH).getPath() + "/data", ',', '"', true);
-        initializeDatabaseFromExperimentFolder(EXPERIMENT_FOLDER_PATH, database, schema, true);
+        initializeDatabaseFromExperimentFolder(EXPERIMENT_FOLDER_PATH, database, schema);
 
         // Define the variants
         ExperimentVariant q0 = ExperimentVariant.builder()
                 .queryId("Q0")
-                .querySQL("SELECT a.artistId, a.name FROM artists a WHERE a.birthYear = 1941")
-                .queryUDF("SELECT a.artistId, a.name FROM artists a WHERE udfilter('Is the artist called {1} born in 1941?', a.name)")
+                .querySQL("SELECT d.breed FROM dog_breeds d WHERE d.country_of_origin = 'England'")
+                .queryUDF("SELECT d.breed FROM dog_breeds d WHERE udfilter('Is the dog breed called {1} originated from England?', d.breed)")
                 .build();
         ExperimentVariant q1 = ExperimentVariant.builder()
                 .queryId("Q1")
-                .querySQL("SELECT a.artistId, a.name FROM artists a WHERE a.nationality = 'American'")
+                .querySQL("SELECT d.breed FROM dog_breeds d WHERE POSITION('Black' IN d.fur_color) > 0")
                 .queryUDF("SELECT a.artistId, a.name FROM artists a WHERE udfilter('Is the artist called {1} American?', a.name)")
                 .build();
 
-        variants = List.of(q0,q1);
+        variants = List.of(q0);
     }
 
     @Test
@@ -85,7 +85,7 @@ public class TestEvaluationArtists {
             IAlgebraOperator gtOperator = sqlQueryParser.parse(variant.getQuerySQL());
             List<Tuple> expected = toTupleList(gtOperator.execute(database, database));
             log.info("**** Expected: {}", expected);
-
+/*
             IAlgebraOperator operator = new SQLQueryParser().parse(variant.getQueryUDF(), GALOIS_UDF_FACTORY);
             List<Tuple> results = TestUtils.toTupleList(operator.execute(database, database));
             log.info("**** Result: {}", results);
@@ -94,6 +94,7 @@ public class TestEvaluationArtists {
                 Double score = metric.getScore(database, expected, results);
                 log.info("**** {}: {} has score {}", variant.getQueryId(), metric.getName(), score);
             }
+*/
         }
     }
 }
