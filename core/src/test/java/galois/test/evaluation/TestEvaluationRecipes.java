0cae67d27480e9d718f9739b040564dcf4c73afd
andreaGaudino <andrea.gaudino02@gmail.com>
2026-01-21 11:50:39 +0100

    Update TestEvaluationRecipes.java

--- core/src/test/java/galois/test/evaluation/TestEvaluationRecipes.java
+++ core/src/test/java/galois/test/evaluation/TestEvaluationRecipes.java
@@ -2,6 +2,8 @@ package galois.test.evaluation;
 
 import com.galois.sqlparser.IUserDefinedFunctionFactory;
 import com.galois.sqlparser.SQLQueryParser;
+
+import galois.llm.query.LLMQueryStatManager;
 import galois.test.experiments.metrics.*;
 import galois.test.utils.ExcelExporter;
 import galois.test.utils.TestRunner;
@@ -77,22 +79,33 @@ public class TestEvaluationRecipes {
 
     @Test
     public void testEvaluation() {
+        EvaluationResults evaluationResults = new EvaluationResults();
         SQLQueryParser sqlQueryParser = new SQLQueryParser();
+
         for (ExperimentVariant variant : variants) {
-            log.info("Parsing query {}", variant.getQueryId());
+            LLMQueryStatManager.getInstance().resetStats();
+
+            long startTime = System.currentTimeMillis();
             IAlgebraOperator gtOperator = sqlQueryParser.parse(variant.getQuerySQL());
             List<Tuple> expected = toTupleList(gtOperator.execute(database, database));
-            log.info("**** Expected: {}", expected);
-
             IAlgebraOperator operator = new SQLQueryParser().parse(variant.getQueryUDF(), GALOIS_UDF_FACTORY);
-            List<Tuple> results = TestUtils.toTupleList(operator.execute(database, database));
-            log.info("**** Result: {}", results);
+            List<Tuple> results = toTupleList(operator.execute(database, database));
 
-            for (IMetric metric : DEFAULT_METRICS) {
-                Double score = metric.getScore(database, expected, results);
-                log.info("**** {}: {} has score {}", variant.getQueryId(), metric.getName(), score);
-            }
+            EvaluationResult evaluationResult = new EvaluationResult(
+                    EXPERIMENT_NAME,
+                    variant,
+                    startTime,
+                    expected,
+                    results,
+                    DEFAULT_METRICS
+            );
+            evaluationResult.computeScores(database);
+            evaluationResults.appendResult(evaluationResult);
+            log.info("**** {}", evaluationResult);
         }
+
+        evaluationResults.exportAsText(EXPERIMENT_NAME);
+        evaluationResults.exportAsExcel(EXPERIMENT_NAME);
     }
 }
 
